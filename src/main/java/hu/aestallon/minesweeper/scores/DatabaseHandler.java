434fb4d71c7c431ef2ea487203d155ae50e9610e
Papp Szabolcs <papp.szabolcs.bazil@gmail.com>
2022-07-05 22:29:36 +0200

    Add sqlite support for score storage
    - The POM now includes the sqlite dependency and the assembly instructions to include its jar to the final executable
    - DatabaseHandler provides a mockable singleton as an interface to the database.
    - Player describes the current player and offers quick in-memory look-up for his/her previous scores.
    - ScoreCategory lists the distinguished types into which score results can fall.

---
+++ src/main/java/hu/aestallon/minesweeper/scores/DatabaseHandler.java
@@ -0,0 +1,152 @@
+package hu.aestallon.minesweeper.scores;
+
+import hu.aestallon.minesweeper.Main;
+
+import java.io.File;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHandler {
+
+    private static final String DB_HEADER = "jdbc:sqlite:";
+    private static final String DB_NAME = "/minesweeper.db";
+
+    private final String dbUrl;
+
+    public static DatabaseHandler getInstance() {
+        return new DatabaseHandler(DB_HEADER, getInstallDirectory(), DB_NAME);
+    }
+
+    @SuppressWarnings("unused")
+    public static DatabaseHandler getCustomInstance(String dbHeader, String dbDirectory, String dbName) {
+        return new DatabaseHandler(dbHeader, dbDirectory, dbName);
+    }
+
+    private static String getInstallDirectory() {
+        String jarPath = Main.class
+                .getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .getPath();
+        return new File(jarPath)
+                .getParentFile()
+                .getPath();
+    }
+
+    private DatabaseHandler(String dbHeader, String dbDirectory, String dbName) {
+        dbUrl = dbHeader + dbDirectory + dbName;
+    }
+
+    private Connection connect() throws SQLException {
+        return DriverManager.getConnection(dbUrl);
+    }
+
+
+    public void initDatabase() {
+        final String sql = """
+                CREATE TABLE IF NOT EXISTS scores(
+                    id     integer      PRIMARY KEY,
+                    player varchar(255) NOT NULL,
+                    score  integer      NOT NULL
+                );""";
+        try (Connection conn = connect()) {
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clear() {
+        final String sql = "DELETE FROM scores;";
+        try (Connection conn = connect()) {
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int insertScore(String name, int score) {
+        final String sql = """
+                INSERT INTO scores (player, score)
+                VALUES (?,?);""";
+        try (Connection conn = connect()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, name);
+            stmt.setInt(2, score);
+            return stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public List<Integer> getPlayerScoresOrdered(String name) {
+        final String sql = """
+                SELECT   s.score AS "SCORE"
+                FROM     MAIN.scores s
+                WHERE    UPPER(s.player) = UPPER(?)
+                ORDER BY s.score;""";
+        List<Integer> playerScores = new ArrayList<>();
+        try (Connection conn = connect()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, name);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                playerScores.add(rs.getInt("SCORE"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return playerScores;
+    }
+
+    public int getHighestScore() {
+        final String sql = """
+                SELECT  MAX(s.score) AS "BEST"
+                FROM    MAIN.scores s;""";
+        Integer highestScore = null;
+        try (Connection conn = connect()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) highestScore = rs.getInt("BEST");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return (highestScore == null) ? 0 : highestScore;
+    }
+
+    public List<Score> getTopTenScores() {
+        final String sql = """
+                SELECT
+                    s.player    AS "NAME",
+                    s.score     AS "SCORE"
+                FROM
+                    MAIN.scores s
+                ORDER BY
+                    s.score DESC
+                LIMIT 10;""";
+        List<Score> scores = new ArrayList<>();
+        try (Connection conn = connect()) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                String name = rs.getString("NAME");
+                Integer score = rs.getInt("SCORE");
+                Score s = new Score(name, score);
+                scores.add(s);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return scores;
+    }
+
+    public record Score(
+            String name,
+            Integer score
+    ) {
+    }
+}
---
+++ src/main/java/hu/aestallon/minesweeper/scores/Player.java
@@ -0,0 +1,62 @@
+package hu.aestallon.minesweeper.scores;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public final class Player {
+
+    private final String name;
+    private List<Integer> scores;
+    private final DatabaseHandler databaseHandler;
+
+    private int personalBest;
+
+    private long startTime;
+    private long endTime;
+    private int currentScore;
+
+    public Player(String name, DatabaseHandler databaseHandler) {
+        this.name = Objects.requireNonNull(name);
+        this.databaseHandler = Objects.requireNonNull(databaseHandler);
+
+        scores = databaseHandler.getPlayerScoresOrdered(name);
+        if (scores.isEmpty()) {
+            personalBest = 0;
+        } else {
+            Collections.sort(scores);
+            personalBest = scores.get(scores.size() - 1);
+        }
+    }
+
+    public ScoreCategory saveScore(int score) {
+        ScoreCategory category;
+        if (score > databaseHandler.getHighestScore()) {
+            personalBest = score;
+            category = ScoreCategory.HIGH_SCORE;
+        } else if (score > personalBest){
+            personalBest = score;
+            category = ScoreCategory.PERSONAL_BEST;
+        } else {
+            category = ScoreCategory.REGULAR;
+        }
+        scores.add(score);
+        databaseHandler.insertScore(this.name, score);
+        return category;
+    }
+
+    public void startGame() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public ScoreCategory endGame(int mineCount) {
+        this.endTime = System.currentTimeMillis();
+        long gameTime = endTime - startTime;
+        int score = calculateScore(gameTime, mineCount);
+        return this.saveScore(score);
+    }
+
+    private int calculateScore(long gameTime, int mineCount) {
+        return (int) (mineCount * 10_000_000 / gameTime);
+    }
+}
